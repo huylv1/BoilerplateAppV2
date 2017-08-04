d443051bb08c503343fbfdfb3443abc712f7b14a
Le Van Huy <huylv1@itsol.vn>
2017-08-04 10:36:12 +0700

    Added boilerplate files

---
+++ app/components/Layout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+
+const Layout = () => (
+    <div>        
+        <div className="grid-x grid-padding-x small-up-2 medium-up-2 large-up-2 large-offset-4">
+            <div className="cell text-center">                                                
+                    Layout.jsx
+                    <Switch>                        
+                    </Switch>                
+            </div>            
+        </div>        
+    </div>
+);
+
+module.exports = Layout;
